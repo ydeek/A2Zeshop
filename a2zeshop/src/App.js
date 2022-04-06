eca1b5c93187967e252b26e929f71f3719129a00
Yazan Deek <ydeek91@yahoo.com>
2022-04-05 17:10:43 -0700

    wrote divs code for the header component in App.js

--- a2zeshop/src/App.js
+++ a2zeshop/src/App.js
@@ -3,8 +3,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="header_logo">
+
+    </div>
+
+    <div className="header_search">
+
+    </div>
+
+    <div className="header_nav">
+      <div className="nav_item">
+       <span className="nav_itemLineOne">Hello Guest</span>
+       <span className="nav_itemLineTwo">Sign In</span>
+      </div>
+
+      <div className="nav_item">
+      <span className="nav_itemLineOne">Your</span>
+       <span className="nav_itemLineTwo">Shop</span>
+      </div>
+
+      <div className="nav_item">
+      <span className="nav_itemLineOne">Basket</span>
+       <span className="nav_itemLineTwo">0</span>
+      </div>
+
+    
+    </div>
+  
     </div>
   );
 }
 
 export default App;
+
